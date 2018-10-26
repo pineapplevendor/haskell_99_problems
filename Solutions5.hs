a9b7304ad997234adf4ca42a2925da672f830115
echavis <echavis@umich.edu>
2018-10-26 00:37:48 -0400

    Add all 1 of the problems I'm gonna do in the 5th set

---
+++ Solutions5.hs
@@ -0,0 +1,22 @@
+module Solutions5
+(grayCode,
+) where
+
+import Solutions1
+import Solutions2
+import Solutions3
+import Solutions4
+
+--problem 46, 47, and 48 are uninteresting 
+
+--problem 49
+grayCode :: Int -> [[Char]]
+grayCode 2 = ["00","01","11","10"]
+grayCode n = 
+    let prev = grayCode (n-1)
+        reflected = reverse prev
+        prefix0 = map ('0':) prev
+        prefix1 = map ('1':) reflected
+    in prefix0 ++ prefix1
+
+--problem 50 (unclear instructions)
